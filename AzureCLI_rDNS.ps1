b15b1f40a804d13b231954573ff6ce1bdb6562c1
Jonathan Sloan <jsloan117@gmail.com>
2018-08-19 19:03:26 -0500

    removed the ability to auto-set IP to static. Figured it'd be good if you did it manually via Azure Portal.

--- AzureCLI_rDNS.ps1
+++ AzureCLI_rDNS.ps1
@@ -1,41 +1,48 @@
 <# sets reverse DNS in Azure
 07/29/18
 Jonathan Sloan <jsloan117@gmail.com>
-version: 1.0
+version: 1.1
 #>
 
+# Force executionpolicy to Bypass (no prompt)
+Write-Output "Forcing ExecutionPolicy to Bypass for only this session to execute commands correctly.`n"
+Set-Executionpolicy -Scope Process -ExecutionPolicy ByPass -Force
+
 # Set Shell Window Title
-$Shell = $Host.UI.RawUI; $Shell.WindowTitle="Azure reverse DNS Tool"
+$Shell = $Host.UI.RawUI; $Shell.WindowTitle="AzureCLI reverse DNS Tool"
+
+# set article for help
+$ip_kb = 'https://docs.microsoft.com/en-us/azure/virtual-network/virtual-network-public-ip-address#view-change-settings-for-or-delete-a-public-ip-address'
 
 # Login as your Azure Client
-az login
+az login; Write-Output "`nMay need to press enter to continue.`n"
 
 # Get ResourceGroups
-Clear-Host; az group list --output table
+az group list --output table
 
 # Prompt for RG
 Write-Output "`n"; $rg = Read-Host -Prompt 'Enter Resource Group Name: '
 
-function SetrDNS () {
-    Write-Output "`n"; az network public-ip list -g $rg --output table
-    Write-Output "`n"; $pipname = Read-Host -Prompt 'Enter PublicIP Name: '
-    Write-Output "`n"; $hname = Read-Host -Prompt 'Enter a unique name not FQDN: '
-    Write-Output "`n"; $fname = Read-Host -Prompt 'Enter Reverse DNS record name: '
-    Write-Output "`n"; az network public-ip list -g $rg --output table | findstr /L Static > $null; $rcode = $?
+function Check_Allocation_Method () {
+  Write-Output "`n"; az network public-ip list -g $rg --output table | findstr /L Dynamic > $null; $rcode = $?
 
-    if ($rcode = 'True') {
-        $pipam = 'Static'
-    } else {
-        $pipam = 'Dynamic'
-    }
-
-    if ($pipam -notmatch '^Static$') { # Ensure PublicIP is assigned statically - may cause a reboot of the VM
-        Write-Output "Setting Public Allocation Method to Static and proceeding with rDNS setup.`n"
-        az network public-ip update -g $rg -n $pipname --dns-name $hname --allocation-method Static --reverse-fqdn "${fname}." --output table > $null
-    } else {
-        Write-Output "Public IP assigned statically. Proceeding with rDNS setup.`n"
-        az network public-ip update -g $rg -n $pipname --dns-name $hname --reverse-fqdn "${fname}." --output table > $null
-    }
+  if ($rcode -match 'True') {
+    Clear-Host; Write-Output "`n";
+    Write-Output "Public IP is Dynamically assigned. Please change within your https://poral.azure.com account, this article should help. `n`n$ip_kb`n`n"
+    Read-Host -Prompt 'Press ENTER to exit. '
+    exit 1
+  }
 }
+
+function SetrDNS () {
+  az network public-ip list -g $rg --output table
+  Write-Output "`n"; $pipname = Read-Host -Prompt 'Enter PublicIP Name: '
+  Write-Output "`n"; $hname = Read-Host -Prompt 'Enter a unique name not FQDN: '
+  Write-Output "`n"; $fname = Read-Host -Prompt 'Enter Reverse DNS record name: '
+
+  Write-Output "Setting up reverse DNS.`n"
+  az network public-ip update -g $rg -n $pipname --dns-name $hname --reverse-fqdn "${fname}." --output table > $null
+}
+Check_Allocation_Method
 SetrDNS
-Write-Output "`n"; Read-Host -Prompt 'Press ENTER to finish and exit'
+Write-Output "`n"; Read-Host -Prompt 'Press ENTER to finish and exit '
--- README.md
+++ README.md
@@ -2,8 +2,12 @@
 
 **Using one of these on an Operating System not shown next to them may result in failure.**
 
-- AzureRM_rDNS.ps1 -- Set reverse DNS using AzureRM Powershell module. (Windows)
+How to change your IP from Dynamic to Static: <https://docs.microsoft.com/en-us/azure/virtual-network/virtual-network-public-ip-address#view-change-settings-for-or-delete-a-public-ip-address>
 
-- AzureCLI_rDNS.ps1 -- Set reverse DNS using Azure-CLI 2.0 (Windows)
+If you have 2FA (Two-Factor auauthentication) enabled on your Azure account do **_NOT_** use the AzureRM. <https://docs.microsoft.com/en-us/powershell/module/azurerm.profile/add-azurermaccount?view=azurermps-4.4.1#description>
 
-- AzureCLI_rDNS.sh -- Set reverse DNS using Azure-CLI 2.0 (Linux/Mac)
+- `AzureRM_rDNS.ps1` -- Set reverse DNS using AzureRM Powershell module. (Windows)
+
+- `AzureCLI_rDNS.ps1` -- Set reverse DNS using Azure-CLI 2.0 (Windows)
+
+- `AzureCLI_rDNS.sh` -- Set reverse DNS using Azure-CLI 2.0 (Linux/Mac)
